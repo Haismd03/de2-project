226b1477aae06b8db2d58b2749824c8ae9dad580
David Haisman <haisman.david@seznam.cz>
2025-12-02 17:58:17 +0100

    Debouncng protection

--- de2-project/src/example/rotary_encoder.c
+++ de2-project/src/example/rotary_encoder.c
@@ -6,6 +6,7 @@
 #include "encoder.h"
 #include "stdio.h"
 #include "uart.h"
+#include "millis.h"
 
 #define LED_BUILTIN PB5  // On-board LED
 #define BUTTON_PIN PB0  // On-board Button
@@ -18,6 +19,8 @@ int main(void) {
     gpio_mode_input_pullup(&DDRB, BUTTON_PIN);
 
     uart_init(UART_BAUD_SELECT(9600, F_CPU));
+
+    millis_init();
 
     uint16_t enc1_position = 0;
     uint16_t enc2_position = 0;
@@ -35,14 +38,16 @@ int main(void) {
 
     uint8_t var_number = 0;
     uint8_t button_last_state = 0;
+    uint32_t button_last_change_ms = 0;
 
     while(1) {
 
         uint8_t button_state = gpio_read(&PINB, BUTTON_PIN);
-        if (button_state == 1 && button_last_state == 0) {
+        if (button_state == 1 && button_last_state == 0 && (millis() - button_last_change_ms) > 10) {
             var_number ^= 1;
 
             encoder.settings_p = (var_number == 0) ? &settings_1 : &settings_2;
+            button_last_change_ms = millis();
         }
         button_last_state = button_state;
 
@@ -51,7 +56,7 @@ int main(void) {
         
         asm volatile("" ::: "memory");
         if (*(encoder.settings_p->position_p) != last_position/* || encoder_2.position != last_position_2*/) {
-            sprintf(uart_msg, "Encoder position: \t1: %d \talias 1: %d \talias 2: %d \tsettings: %d\n", *(encoder.settings_p->position_p), enc1_position, enc2_position, var_number);
+            sprintf(uart_msg, "Encoder position: \t1: %d \talias 1: %d \talias 2: %d \tsettings: %d \tmillis: %lu\n", *(encoder.settings_p->position_p), enc1_position, enc2_position, var_number, millis());
             //sprintf(uart_msg, "Encoder position: \t1: %d \t2: %d \talias: %d\n", encoder.position, encoder_2.position, *enc_allias_p);
             uart_puts(uart_msg);
             last_position = *(encoder.settings_p->position_p);
