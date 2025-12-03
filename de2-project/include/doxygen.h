6e7f71174f9b0d1c77ac02c97b4feeedf39ee939
David Haisman <haisman.david@seznam.cz>
2025-12-03 23:11:17 +0100

    Add doxygen.g

---
+++ de2-project/include/doxygen.h
@@ -0,0 +1,22 @@
+/**
+ * @defgroup external_libraries External Libraries
+ * @brief External libraries used in this project.
+ * @{
+ */
+
+/** @} */ // end of external_libraries
+
+/** @defgroup custom_libraries Custom Libraries
+ * @brief Custom libraries developed by project members.
+ * @{
+ */
+
+/** @} */ // end of custom_libraries
+
+/**
+ * @defgroup project_files Project Files
+ * @brief Project-specific source code
+ * @{
+ */
+
+/** @} */ // end of project_files
